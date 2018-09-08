db689c625928b16041a1e5f402554fd9d652eb90
Jiadao Zou <jiadao.zou@outlook.com>
2018-09-08 13:17:55 -0500

    CS6347

---
+++ CS6347PGM/test/test.py
@@ -0,0 +1,49 @@
+script_dir = sys.path[0]
+# %matplotlib inline
+#
+# from __future__ import division
+#
+# import os
+# import sys
+# import tensorflow as tf
+# import skimage.io as io
+# import numpy as np
+#
+# os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+# sys.path.append("/Users/jiadao/PycharmProjects/untitled/models/research/slim")
+# checkpoints_dir = '/Users/jiadao/PycharmProjects/untitled/checkpoints'
+#
+# image_filename = '/Users/jiadao/PycharmProjects/untitled/PGM/data/cat.jpg'
+# annotation_filename = '/Users/jiadao/PycharmProjects/untitled/PGM/data/cat_annotation.png'
+#
+# image_filename_placeholder = tf.placeholder(tf.string)
+# annotation_filename_placeholder = tf.placeholder(tf.string)
+# is_training_placeholder = tf.placeholder(tf.bool)
+#
+# feed_dict_to_use = {image_filename_placeholder: image_filename,
+#                     annotation_filename_placeholder: annotation_filename,
+#                     is_training_placeholder: True}
+#
+# image_tensor = tf.read_file(image_filename_placeholder)
+# annotation_tensor = tf.read_file(annotation_filename_placeholder)
+#
+# image_tensor = tf.image.decode_jpeg(image_tensor, channels=3)
+# annotation_tensor = tf.image.decode_png(annotation_tensor, channels=1)
+#
+# # Get ones for each class instead of a number -- we need that
+# # for cross-entropy loss later on. Sometimes the groundtruth
+# # masks have values other than 1 and 0.
+# class_labels_tensor = tf.equal(annotation_tensor, 1)
+# background_labels_tensor = tf.not_equal(annotation_tensor, 1)
+#
+# # Convert the boolean values into floats -- so that
+# # computations in cross-entropy loss is correct
+# bit_mask_class = tf.to_float(class_labels_tensor)
+# bit_mask_background = tf.to_float(background_labels_tensor)
+#
+# combined_mask = tf.concat(concat_dim=2, values=[bit_mask_class,
+#                                                 bit_mask_background])
+#
+# # Lets reshape our input so that it becomes suitable for
+# # tf.softmax_cross_entropy_with_logits with [batch_size, num_classes]
+# flat_labels = tf.reshape(tensor=combined_mask, shape=(-1, 2))
